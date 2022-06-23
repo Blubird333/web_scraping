0ec6bfe82bbdc6fd85eafd2b5de06072f88a4885
Chinthakunta Raj Dheeraj Reddy <rajdheerajcreddy@gmail.com>
2022-06-23 10:32:49 +0530

    using beautiful soup functions for the content of the webpage to organize the information of the webpage that is being scraped.

--- README.md
+++ README.md
@@ -10,4 +10,7 @@
 3. Then activate the virtualenv using '. /tmp/mscraper/bin/activate'.
    You can deactivate it by typing 'deactivate' and hitting enter.
 
-4. 
+## Installing packages
+1. After activating your virtualenv, use 'pip install requests' to install the requests library.
+
+2. 
--- scraper.py
+++ scraper.py
@@ -1,7 +1,19 @@
 # scraper for lyrics of stay by zed from metrolyrics.com
 
 import requests
+import bs4
 
 lyrics= requests.get("https://metrolyrics.pro/lyrics/zedd-alessia-cara/stay-lyrics-nyuj8sbi.html").content
 
-print(lyrics)
+soup = bs4.BeautifulSoup(lyrics,features="lxml")
+
+all_a_tags = soup.find_all("a",{"class":"button"})
+
+#all 'a' tags in with the class 'button'.
+for i in all_a_tags:
+    print(i)
+print("**********************************************************************************************************")
+
+#links for all 'a' tags with the class 'button'
+for i in all_a_tags:
+    print(i['href'])
